41c701ce776aa7005e4637cdfb7e6d010f7ec992
Karlanicol06 <karla.gutierrez1701@alumnos.udg.mx>
2025-05-15 20:04:14 -0600

    Update main.c
    cambios de Karla Archivo punto c

--- main.c
+++ main.c
@@ -1,12 +1,14 @@
- #include <stdio.h>
+#include <stdio.h>
 #include "practicaja.h"
 
 int main() {
     int cuentaSeleccionada;
     int operaciones;
     int bucle = 1;
+    int numOname;
+    char nameCliente[40];
+    int a, b, c, data=0;
 
- 
     struct cliente banco[100] = {
         {1, 15000, "Carlos", "Ramirez", 1234},
         {2, 10000, "Karla","Gutierrez", 1701},
@@ -14,25 +16,67 @@ int main() {
     };
 
     int n_usuarios = sizeof(banco)/sizeof(banco[0]);
+    while(1)
+    {
     printf("\n========== BIENVENIDO A LA PRACTICAJA CUCEI ==========\n");
 
-    printf("\nPor favor seleccione la cuenta en la que quiere realizar una operación:\n");
-    printf("1.- Cuenta 1\n");
-    printf("2.- Cuenta 2\n");
-    printf("3.- Cuenta 3\n");
-    scanf("%d", &cuentaSeleccionada);
+    printf("ingrese con que opcion desea ingresar a su cuenta:\n");
+        printf("1.- ingresar con numero de cliente\n");
+        printf("2.-ingresar con su nombre\n");
+        printf("3.-Salir de la practicaja\n");
+        scanf("%d",&numOname);
 
-    if (cuentaSeleccionada < 1 || cuentaSeleccionada > 3) {
-        printf("La opcion ingresada no es válida.\n");
-        return 0;
-    }
+        switch(numOname)
+        {
+            case 1:
+            printf("por favor ingrese su numero de cliente para acceder a su cuenta: \n");
+            scanf("%d", &cuentaSeleccionada);
+            if (cuentaSeleccionada < 1 || cuentaSeleccionada > 3)
+            {
+                    printf("La opcion ingresada no es valida.\n");
+                    return 0;
+            }
 
-    cuentaSeleccionada -= 1;
+            cuentaSeleccionada -= 1;
 
-    if (!autenticarNip(banco, cuentaSeleccionada)) {
-        return 0;
-    }
+            if (!autenticarNip(banco, cuentaSeleccionada))
+            {
+                return 0;
+            }
+            break;
 
+            case 2:
+            printf("Ingrese su nombre (un nombre y un apellido): \n");
+            getchar();
+            fgets(nameCliente, sizeof(nameCliente), stdin);
+            nameCliente[strcspn(nameCliente, "\n")] = 0;
+            data = 0;
+            for (a = 0; a < 3; a++)
+            {
+                if (compararNombreCompleto(banco[a], nameCliente))
+                {
+                    printf("Su cuenta fue encontrada.\n");
+                    cuentaSeleccionada = a;
+                    data = 1;
+
+                    if (!autenticarNip(banco, cuentaSeleccionada))
+                    {
+                        return 0;
+                    }
+                    break;
+                }
+            }
+
+            if (data == 0)
+            {
+                printf("Su cuenta no fue encontrada.\n");
+                return 0;
+            }
+            break;
+            case 3:
+                 printf("\nGracias por usar la Practicaja CUCEI. Que tenga un buen dia\n");
+                    return 0;
+        }
     do {
         printf("\nPor favor ingrese la opcion deseada:\n\n");
         printf("1.- Consulta de saldo\n");
@@ -59,10 +103,15 @@ int main() {
         }
 
         printf("\nDesea realizar alguna otra operacion?\n");
-        printf("1.- Si\n");
-        printf("2.- No\n");
-        scanf("%d", &bucle);
-    } while (bucle != 2);
+            printf("1.- Si\n");
+            printf("2.- No, regresar al menu principal\n");
+            scanf("%d", &bucle);
+            if (bucle == 2)
+            {
+                break;
+            }
+        } while (bucle == 1);
+    }
 
     printf("\nGracias por usar la Practicaja CUCEI. Que tenga un buen dia\n");
     return 0;
